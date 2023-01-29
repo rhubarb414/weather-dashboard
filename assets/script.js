602fdecfb4eb395ae089a7597aa239b68f66dba2
rhubarb414 <ctophr.dubois@gmail.com>
2023-01-28 17:44:35 -0800

    adds links to js, css, and bulma to index

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var apiKey = a9c922718c1bf79be61715d73fa7a803;
+var lat;
+var long;
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`; //forecast?
