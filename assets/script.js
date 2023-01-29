8467761005d3b402b1ebf441f13cc8f3a017342b
rhubarb414 <ctophr.dubois@gmail.com>
2023-01-28 20:23:23 -0800

    Adds getForecast(), sucessfully fetches forecast

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
 var apiKey = "a9c922718c1bf79be61715d73fa7a803";
 var lat;
 var long;
+var queryURL;
 var city;
 var stateFull;
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`; //forecast?
+// var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`; //forecast?
 
 var searchInputVal = document.querySelector(".search-input").value;
 var searchBtn = document.querySelector(".search-button");
@@ -18,6 +19,7 @@ searchBtn.addEventListener("click", function (event) {
   stateAbbr = cityStateArr[1].trim();
 
   convertToCoords(city, stateAbbr);
+  //   getForecast();
 });
 
 // takes city, state input from user and converts to coordinates
@@ -33,8 +35,21 @@ function convertToCoords(city, stateAbbr) {
       lat = data[0]["lat"];
       long = data[0]["lon"];
       console.log("lat = " + lat + " long = " + long);
+      queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
+      console.log(queryURL);
       city = data[0]["name"];
-      stateFull = data[0]["state"]; //store spelled-out state to display on DOM, in case user confuses abbreviation
+      stateFull = data[0]["state"]; //store full state name to display on DOM, in case user confuses abbreviation
       console.log(stateFull);
+      getForecast(queryURL);
+    });
+}
+// api call for weather forecast
+function getForecast(queryURL) {
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
     });
 }
