67509ebfeb17465fb5af744ef933d897fac94c34
rhubarb414 <ctophr.dubois@gmail.com>
2023-02-05 18:02:17 -0800

    adds all current condition data, reforamts js to let,const, arrow functions

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,27 @@
-var APIKey = "a9c922718c1bf79be61715d73fa7a803";
-var lat;
-var long;
-var queryURL;
-var city;
-var stateFull;
-let currentHeaderEl = document.querySelector(".current-header");
-var searchInputVal = document.querySelector(".search-input").value;
-var searchBtn = document.querySelector(".search-button");
+const APIKey = "a9c922718c1bf79be61715d73fa7a803";
+let lat;
+let long;
+let queryURL;
+let city;
+let stateFull;
+const currentHeaderEl = document.querySelector(".current-header");
+const currentIconEl = document.querySelector(".current-icon");
+const searchBtn = document.querySelector(".search-button");
 
 searchBtn.addEventListener("click", function (event) {
   //city and state to be define by user input
-  var cityStateArr = [];
+  let cityStateArr = [];
 
-  searchInputVal = document.querySelector(".search-input").value;
+  const searchInputVal = document.querySelector(".search-input").value;
   cityStateArr = searchInputVal.split(",");
   city = cityStateArr[0];
   stateAbbr = cityStateArr[1].trim();
 
   convertToCoords(city, stateAbbr);
-  //   getForecast();
 });
 
 // takes city, state input from user and converts to coordinates
-function convertToCoords(city, stateAbbr) {
+const convertToCoords = (city, stateAbbr) => {
   fetch(
     `https://api.openweathermap.org/geo/1.0/direct?q=${city},${stateAbbr},US&limit=1&appid=${APIKey}`
   )
@@ -38,11 +37,11 @@ function convertToCoords(city, stateAbbr) {
       queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,alerts&appid=${APIKey}`;
 
       //make api call to get forecast
-      getForecast();
+      callAPI();
     });
-}
+};
 // api call for weather forecast
-function getForecast() {
+const callAPI = () => {
   let currentDate = "";
   let currentTime = "";
   let localTimezone = "";
@@ -53,15 +52,30 @@ function getForecast() {
     })
     .then(function (data) {
       console.log(data); // delete
-      //multiply current dt by 1000 as dayjs uses unix timestamp in milliseconds as standard
-      localTimezone = data.timezone_offset;
-      currentDate = dayjs
-        .utc((data.current.dt + localTimezone) * 1000)
-        .format("dddd MMM DD");
-      currentTime = dayjs
-        .utc((data.current.dt + localTimezone) * 1000)
-        .format("h:mm A");
-      console.log("currentDate is " + currentDate); //delete
-      currentHeaderEl.textContent = `Conditions in ${city}, ${stateFull} at ${currentTime}, ${currentDate}.`;
+      currentConditions(data);
     });
-}
+};
+
+const currentConditions = (data) => {
+  //multiply current dt by 1000 as dayjs uses unix timestamp in milliseconds as standard
+  localTimezone = data.timezone_offset;
+
+  currentDate = dayjs
+    .utc((data.current.dt + localTimezone) * 1000) //use utc plugin so dayjs doesn't display local machine time
+    .format("dddd MMM DD");
+  currentTime = dayjs
+    .utc((data.current.dt + localTimezone) * 1000)
+    .format("h:mm A");
+
+  currentHeaderEl.textContent = `Conditions in ${city}, ${stateFull} at ${currentTime}, ${currentDate}.`;
+  const iconURL = `<img src=http://openweathermap.org/img/wn/${
+    data.current.weather[0].icon
+  }@2x.png></img>
+   <p>${data.current.weather[0].description}</p> 
+  <p>${Math.floor(data.current.temp)}\u{00B0}F</p>
+  <p>${data.current.humidity}% humidity</p>
+  <p>${Math.floor(data.current.wind_speed)} mph wind`;
+  currentIconEl.innerHTML = iconURL;
+};
+
+const getForecast = (data) => {};
